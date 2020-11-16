85e9652afd0d02fee89085f9d75bebfb64ccf112
Htsubasa <57310291+Htsubasa@users.noreply.github.com>
2020-11-17 02:05:31 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,3 +1,19 @@
+$(function(){
+    // #で始まるアンカーをクリックした場合に処理
+        $('a[href^="#"]').click(function() {
+            // スクロールの速度
+            var speed = 500; // ミリ秒
+            // 移動先を取得
+            var href= $(this).attr("href");
+            var target = $(href == "#" || href == "" ? 'html' : href);
+            // 移動先を数値で取得
+            var position = target.offset().top;
+            // スムーススクロール
+            $('body,html').animate({scrollTop:position}, speed, 'swing');
+            return false;
+        });
+    });
+
 $('.slider').slick({
     autoplay:true,
     autoplaySpeed:3000,
@@ -47,6 +63,7 @@ $(function(){
     });
   });
 
+
 $(function(){
     $('.mv').delay(1500).queue(function(){
     $(this).addClass('mvactive').dequeue();
@@ -79,4 +96,27 @@ $(function(){
 	    }else{
 	        target.removeClass('show');
 	    }
-	}
+  }
+
+
+  $(window).on('load scroll resize', function() {
+		//　「section.test02」が存在したら発火
+		if($('.about-fix').length){
+		    add_class_in_scrolling($('.about-fix'));
+		}
+	});
+	// スクロールで要素が表示されている時にclassを付与する
+	function add_class_in_scrolling(target) {
+	    //　指定した要素の次の要素
+	    var nextElement = $('.about-text');
+	    var winScroll = $(window).scrollTop();
+	    var winHeight = $(window).height();
+	    //　ウィンドウの最下部の位置取得
+	    var scrollPos = winScroll + winHeight;
+	    //　指定した要素が画面内に入ったらclass付与して出たら削除
+	    if(target.offset().top - 100 < scrollPos && nextElement.offset().top + winHeight + 100 > scrollPos) {
+	        target.addClass('show');
+	    }else{
+	        target.removeClass('show');
+	    }
+  }
